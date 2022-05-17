2a5d0f47ef1f6fdbea68bad4b00be5f871df4ecd
behilsamuel05@gmail.com <behilalemu@icloud.com>
2022-05-17 16:55:45 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,80 @@
+let facts = {numPlanets: 8, yearNeptuneDiscovered: 1846};
+let {numPlanets, yearNeptuneDiscovered} = facts;
+
+console.log(numPlanets); // 8
+console.log(yearNeptuneDiscovered); // 1846
+
+let planetFacts = {
+    numPlanets: 8,
+    yearNeptuneDiscovered: 1846,
+    yearMarsDiscovered: 1659
+  };
+  
+  let {numPlanets, ...discoveryYears} = planetFacts;
+  
+  console.log(discoveryYears); // {1846, 1659}
+
+  function getUserData({firstName, favoriteColor="green"}){
+    return `Your name is ${firstName} and you like ${favoriteColor}`;
+  }
+  
+  getUserData({firstName: "Alejandro", favoriteColor: "purple"}) // ?
+  getUserData({firstName: "Melissa"}) // Melissa
+  getUserData({}) // Alejandro, purple
+
+  let [first, second, third] = ["Maya", "Marisa", "Chi"];
+
+console.log(first); // Maya
+console.log(second); // Marisa
+console.log(third); // Chi
+
+let [raindrops, whiskers, ...aFewOfMyFavoriteThings] = [
+    "Raindrops on roses",
+    "whiskers on kittens",
+    "Bright copper kettles",
+    "warm woolen mittens",
+    "Brown paper packages tied up with strings"
+  ]
+  
+  console.log(raindrops); // "Raindrops on roses"
+  console.log(whiskers); // "whiskers on kittens"
+  console.log(aFewOfMyFavoriteThings); // ["Bright copper kettles","warm woolen mittens","Brown paper packages tied up with strings"]
+
+  let numbers = [10, 20, 30];
+[numbers[1], numbers[2]] = [numbers[2], numbers[1]]
+
+console.log(numbers) // [20, 10, 30]
+
+var obj = {
+    numbers: {
+      a: 1,
+      b: 2
+    }
+  };
+  
+  var a = obj.numbers.a;
+  var b = obj.numbers.b;
+// ES2015
+let {numbers:{a,b}}= obj
+
+var arr = [1, 2];
+var temp = arr[0];
+arr[0] = arr[1];
+arr[1] = temp;
+
+let arr = [1, 2];
+let one =  arr[0];
+let two =  arr[1];
+[one, two]= [two, one]
+
+raceResults(['Tom', 'Margaret', 'Allison', 'David', 'Pierre'])
+
+let [first, second, third, ...rest] = raceResults
+/*
+  {
+    first: "Tom",
+    second: "Margaret",
+    third: "Allison",
+    rest: ["David", "Pierre"]
+  }
+*/
